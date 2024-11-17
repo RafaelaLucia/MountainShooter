80eb5167982a1bfb025fb0927461a09e1910a5e9
Rafaela Lucia <rafaluciadev@gmail.com>
2024-11-17 18:23:59 -0300

    Criacao da pasta asset - insercao de arquivos - criacao do menu - criacao do arquivo const

---
+++ code/Const.py
@@ -0,0 +1,14 @@
+# C
+COLOR_ORANGE = (255, 128, 0)
+COLOR_WHITE = (255, 255, 255)
+
+# M
+MENU_OPTION = ('NOVO JOGO 1P',
+               'NOVO JOGO 2P - COOPERATIVO',
+               'NOVO JOGO 2P - COMPETITIVO',
+               'PONTUAÇÃO',
+               'SAIR')
+
+# W
+WIN_WIDTH = 800;
+WIN_HEIGHT = 600;
--- code/menu.py
+++ code/menu.py
@@ -1,9 +1,44 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame.image
+from pygame import Surface, Rect
+from pygame.font import Font
+
+from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE
+
 
 class Menu:
     def __init__(self, window):
         self.window = window
+        self.surf = pygame.image.load('./asset/MenuPrincipal.png')
+        self.rect = self.surf.get_rect(left=0, top=0)
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name='Lucida Sans Typewriter', size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
 
     def run(self, ):
-        pass
+        pygame.mixer_music.load('./asset/menu.mp3')
+        pygame.mixer_music.play(-1)
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(50, "Lucky", COLOR_ORANGE, ((WIN_WIDTH / 2), 120))
+            self.menu_text(50, "The Shooter", COLOR_ORANGE, ((WIN_WIDTH / 2), 163))
+
+            for i in range(len(MENU_OPTION)):
+                self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 300 + 30 * i))
+
+
+            pygame.display.flip()
+
+            # Verifica todos os eventos
+            for event in pygame.event.get():
+               if event.type == pygame.QUIT:
+                    print('saindo...')
+                    pygame.quit()  # fecha janela
+                    quit()  # termina pygame
+
+
+
