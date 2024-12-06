5cbb3769f36b6c6f5ed10fcd798f3a65708fbfe3
Rafaela Lucia <rafaluciadev@gmail.com>
2024-12-05 23:07:58 -0300

    criacao das funcionalidades restantes

--- code/Const.py
+++ code/Const.py
@@ -1,11 +1,39 @@
-
+# C
 import pygame
 
-# C
-COLOR_ORANGE = (255, 128, 0)
-COLOR_WHITE = (255, 255, 255)
+C_ORANGE = (255, 128, 0)
+C_YELLOW = (255, 255, 128)
+C_WHITE = (255, 255, 255)
+C_GREEN = (0, 128, 0)
+C_CYAN = (0, 128, 128)
 
 # E
+EVENT_ENEMY = pygame.USEREVENT + 1
+EVENT_TIMEOUT = pygame.USEREVENT + 2
+ENTITY_SPEED = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 1,
+    'Level1Bg2': 2,
+    'Level1Bg3': 3,
+    'Level1Bg4': 4,
+    'Level1Bg5': 5,
+    'Level1Bg6': 6,
+    'Level2Bg0': 0,
+    'Level2Bg1': 1,
+    'Level2Bg2': 2,
+    'Level2Bg3': 3,
+    'Level2Bg4': 4,
+    'Level2Bg5': 5,
+    'Level2Bg6': 6,
+    'Player1': 3,
+    'Player1Shot': 3,
+    'Player2': 3,
+    'Player2Shot': 3,
+    'Enemy1': 2,
+    'Enemy1Shot': 5,
+    'Enemy2': 2,
+    'Enemy2Shot': 2,
+}
 
 ENTITY_HEALTH = {
     'Level1Bg0': 999,
@@ -20,6 +48,8 @@ ENTITY_HEALTH = {
     'Level2Bg2': 999,
     'Level2Bg3': 999,
     'Level2Bg4': 999,
+    'Level2Bg5': 999,
+    'Level2Bg6': 999,
     'Player1': 300,
     'Player1Shot': 1,
     'Player2': 300,
@@ -30,29 +60,69 @@ ENTITY_HEALTH = {
     'Enemy2Shot': 1,
 }
 
-EVENT_ENEMY = pygame.USEREVENT + 1
+ENTITY_DAMAGE = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 0,
+    'Level1Bg2': 0,
+    'Level1Bg3': 0,
+    'Level1Bg4': 0,
+    'Level1Bg5': 0,
+    'Level1Bg6': 0,
+    'Level2Bg0': 0,
+    'Level2Bg1': 0,
+    'Level2Bg2': 0,
+    'Level2Bg3': 0,
+    'Level2Bg4': 0,
+    'Level2Bg5': 0,
+    'Level2Bg6': 0,
+    'Player1': 1,
+    'Player1Shot': 25,
+    'Player2': 1,
+    'Player2Shot': 20,
+    'Enemy1': 1,
+    'Enemy1Shot': 20,
+    'Enemy2': 1,
+    'Enemy2Shot': 15,
+}
 
-ENTITY_SPEED = {
-    'Level1Bg0' : 0,
-    'Level1Bg1' : 2,
-    'Level1Bg2' : 3,
-    'Level1Bg3' : 4,
-    'Level1Bg4' : 5,
-    'Level1Bg5' : 6,
-    'Level1Bg6' : 7,
-    'Player1' : 3,
-    'Player2' : 3,
-    'Enemy1' : 2,
-    'Enemy2' : 1
+ENTITY_SCORE = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 0,
+    'Level1Bg2': 0,
+    'Level1Bg3': 0,
+    'Level1Bg4': 0,
+    'Level1Bg5': 0,
+    'Level1Bg6': 0,
+    'Level2Bg0': 0,
+    'Level2Bg1': 0,
+    'Level2Bg2': 0,
+    'Level2Bg3': 0,
+    'Level2Bg4': 0,
+    'Level2Bg5': 0,
+    'Level2Bg6': 0,
+    'Player1': 0,
+    'Player1Shot': 0,
+    'Player2': 0,
+    'Player2Shot': 0,
+    'Enemy1': 100,
+    'Enemy1Shot': 0,
+    'Enemy2': 125,
+    'Enemy2Shot': 0,
+}
 
+ENTITY_SHOT_DELAY = {
+    'Player1': 20,
+    'Player2': 15,
+    'Enemy1': 100,
+    'Enemy2': 200,
 }
 
 # M
-MENU_OPTION = ('NOVO JOGO 1P',
-               'NOVO JOGO 2P - COOPERATIVO',
-               'NOVO JOGO 2P - COMPETITIVO',
-               'PONTUAÇÃO',
-               'SAIR')
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT')
 
 # P
 PLAYER_KEY_UP = {'Player1': pygame.K_UP,
@@ -63,14 +133,32 @@ PLAYER_KEY_LEFT = {'Player1': pygame.K_LEFT,
                    'Player2': pygame.K_a}
 PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
                     'Player2': pygame.K_d}
-PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
+PLAYER_KEY_SHOOT = {'Player1': pygame.K_LCTRL,
                     'Player2': pygame.K_LCTRL}
 
-#S
+# S
+SPAWN_TIME = 2000
 
-SPAWN_TIME = 4000
-
-
+# T
+TIMEOUT_STEP = 100  # 100ms
+TIMEOUT_LEVEL = 20000  # 20s
 # W
-WIN_WIDTH = 800; #800
-WIN_HEIGHT = 600; #600
+WIN_WIDTH = 800
+WIN_HEIGHT = 600
+
+# S
+SCORE_POS = {'Title': (WIN_WIDTH / 2, 50),
+             'EnterName': (WIN_WIDTH / 2, 80),
+             'Label': (WIN_WIDTH / 2, 90),
+             'Name': (WIN_WIDTH / 2, 110),
+             0: (WIN_WIDTH / 2, 110),
+             1: (WIN_WIDTH / 2, 130),
+             2: (WIN_WIDTH / 2, 150),
+             3: (WIN_WIDTH / 2, 170),
+             4: (WIN_WIDTH / 2, 190),
+             5: (WIN_WIDTH / 2, 210),
+             6: (WIN_WIDTH / 2, 230),
+             7: (WIN_WIDTH / 2, 250),
+             8: (WIN_WIDTH / 2, 270),
+             9: (WIN_WIDTH / 2, 290),
+             }
--- code/menu.py
+++ code/menu.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
 import pygame.image
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE
+from code.Const import WIN_WIDTH, C_ORANGE, MENU_OPTION, C_WHITE, C_YELLOW
 
 
 class Menu:
@@ -13,47 +14,45 @@ class Menu:
         self.surf = pygame.image.load('./asset/MenuPrincipal.png').convert_alpha()
         self.rect = self.surf.get_rect(left=0, top=0)
 
-    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
-        text_font: Font = pygame.font.SysFont(name='Lucida Sans Typewriter', size=text_size)
-        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
-        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
-        self.window.blit(source=text_surf, dest=text_rect)
-
-    def run(self, ):
+    def run(self):
         menu_option = 0
         pygame.mixer_music.load('./asset/menu.mp3')
         pygame.mixer_music.play(-1)
         while True:
-            #desenhar imagens
+            # DRAW IMAGES
             self.window.blit(source=self.surf, dest=self.rect)
-            self.menu_text(50, "Lucky", COLOR_ORANGE, ((WIN_WIDTH / 2), 120))
-            self.menu_text(50, "The Shooter", COLOR_ORANGE, ((WIN_WIDTH / 2), 163))
+            self.menu_text(50, "Lucky", C_ORANGE, ((WIN_WIDTH / 2), 110))
+            self.menu_text(50, "The Shooter", C_ORANGE, ((WIN_WIDTH / 2), 170))
+            self.menu_text(16, "Rafaela Sthefany - RU: 4328824", C_WHITE, ((WIN_WIDTH / 2), 567))
 
             for i in range(len(MENU_OPTION)):
                 if i == menu_option:
-                    self.menu_text(20, MENU_OPTION[i], COLOR_ORANGE, ((WIN_WIDTH / 2), 300 + 30 * i))
+                    self.menu_text(20, MENU_OPTION[i], C_YELLOW, ((WIN_WIDTH / 2), 300 + 25 * i))
                 else:
-                    self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 300 + 30 * i))
+                    self.menu_text(20, MENU_OPTION[i], C_WHITE, ((WIN_WIDTH / 2), 300 + 25 * i))
             pygame.display.flip()
 
-            # Verifica todos os eventos
+            # Check for all events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    print('saindo...')
-                    pygame.quit()  # fecha janela
-                    quit()  # termina pygame
+                    pygame.quit()  # Close Window
+                    quit()  # end pygame
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_DOWN:  # Tecla para baixo
+                    if event.key == pygame.K_DOWN:  # DOWN KEY
                         if menu_option < len(MENU_OPTION) - 1:
                             menu_option += 1
                         else:
                             menu_option = 0
-                    if event.key == pygame.K_UP:  # Tecla para cima
+                    if event.key == pygame.K_UP:  # UP KEY
                         if menu_option > 0:
                             menu_option -= 1
                         else:
                             menu_option = len(MENU_OPTION) - 1
-                    if event.key == pygame.K_RETURN:
-                       return MENU_OPTION[menu_option]
+                    if event.key == pygame.K_RETURN:  # ENTER
+                        return MENU_OPTION[menu_option]
 
-
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
